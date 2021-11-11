60b8328fe58d9f1bcb0d81f86e8a733212ff0590
Bryson Best <bryson.best@gmail.com>
2021-11-11 13:18:47 -0800

    Refactored and nested callbacks to create a profile.

--- survey.js
+++ survey.js
@@ -1,23 +1,24 @@
 //File generates a profile based on asking a series of questions
+//practicing nested callbacks
 
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 
-let profile = {};
-
 const rl = readline.createInterface({ input, output });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  profile['Name'] = answer;// TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+rl.question('What is your name? (or Nickname)? ', (name) => {
+  rl.question('What is an activity you like doing? ', (activity) => {
+    rl.question('What do you listen to while doing that? ', (music) => {
+      rl.question('What is your favourite meal? ', (meal) => {
+        rl.question('What is your favourite thing to eat in that meal? ', (foodItem) => {
+          rl.question('What is your favourite sport? ', (sport) => {
+            rl.question('What is your superpower? That is - what are you amazing at? ', (superpower) => {
+              console.log(`${name} loves listening to ${music} while ${activity}, devouring ${foodItem} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
-
-
-
-
-
-
-console.log(`${profile.Name} loves`);
-
